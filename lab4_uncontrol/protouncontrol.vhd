c0cf5e3742bdca16572d3b888686a6269ea81728
luigi-lipori <luigi.lipori@gmail.com>
2025-10-15 20:54:34 -0300

    Adding new file

---
+++ lab4_uncontrol/protouncontrol.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;   
+
+entity protouncontrol is 
+    port(
+        data_in  : in  std_logic_vector(16 downto 0);  
+        data_out : out std_logic_vector(16 downto 0)
+    )
+end entity protouncontrol;
+
+architecture a_protouncontrol of protouncontrol is
+
+    data_out <= data_in + "00000000000000001";
+
+end architecture a_protouncontrol;
