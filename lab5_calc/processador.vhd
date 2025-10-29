513272ae9e9ad040cfff61883e982f76526f585e
falabrettir <falabretti@alunos.utfpr.edu.br>
2025-10-29 17:21:29 -0300

    COMPILOU

--- lab5_calc/processador.vhd
+++ lab5_calc/processador.vhd
@@ -121,6 +121,88 @@ end component uc;
     signal s_f_zero: std_logic;
     signal s_f_carry:std_logic;
 begin 
-  
+ 
+-- fatiada na instrucao
+  s_opcode <= s_ir_out (16 downto 13);
+  s_reg_dest <= std_logic_vector(s_ir_out(12 downto 9));
+  s_reg_src1 <= std_logic_vector(s_ir_out(8 downto 5));
+  s_reg_src2 <= std_logic_vector(s_ir_out(4 downto 1));
+  s_const_5bit <= s_ir_out(4 downto 0);
+  s_const_13bit <= s_ir_out (12 downto 0);
+
+  s_const_16bit <=unsigned(resize(signed(s_const_5bit), 16)); --por causa dos negativos em cpl 2 
+
+  s_mux_ula_b <= s_const_16bit when s_sel_mux_ula_b = '1' else 
+                 s_dados_r2;
+
+  s_mux_reg_wr <=s_const_16bit when s_sel_mux_reg_wr = '1' else 
+                 s_ula_out;
+
+  inst_pc: pc
+        port map (
+            clk     => clk,
+            rst     => rst,
+            wr_en   => s_pc_wr_en,
+            pc_in   => s_next_pc, -- vem da UC
+            pc_out  => s_pc_out   -- vai pra ROM e UC
+        );
+
+    inst_rom: rom
+        port map (
+            clk      => clk,
+            endereco => s_pc_out(6 downto 0), -- usa apenas os bits necessários para a ROM atual
+            dado     => s_rom_out           -- vai pro IR
+        );
+
+    inst_ir: ir
+        port map (
+            clk     => clk,
+            rst     => rst,
+            wr_en   => s_ir_wr_en, -- vem da UC
+            ir_in   => s_rom_out,  -- vem da ROM
+            ir_out  => s_ir_out    -- vai para fatiamento e UC
+        );
+
+    inst_banco: banco_9_regs
+        port map (
+            clk     => clk,
+            rst     => rst,
+            wr_en   => s_reg_wr_en,            -- vem da UC
+            addr_wr => s_reg_dest,             -- vem do IR fatiado
+            ra1     => s_reg_src1,             -- vem do IR fatiado
+            ra2     => s_reg_src2,             -- vem do IR fatiado
+            data_wr => s_mux_reg_wr,           -- vem do MUX de escrita
+            data_r1 => s_dados_r1,             -- vai para ULA entrada A
+            data_r2 => s_dados_r2              -- vai para MUX B da ULA
+        );
+
+    inst_ula: ula
+        port map (
+            a        => s_dados_r1,    -- vem do Banco (leitura 1)
+            b        => s_mux_ula_b,   -- vem do MUX B
+            chave    => s_ula_chave,   -- vem da UC
+            u_output => s_ula_out,     -- vai para MUX de escrita do Banco
+            f_zero   => s_f_zero,      -- flag (não usada ainda, mas conectada)
+            f_carry  => s_f_carry      -- flag (não usada ainda, mas conectada)
+        );
+
+    inst_uc: uc
+        port map (
+            clk                => clk,
+            rst                => rst,
+            opcode_in          => s_opcode,      -- vem do IR fatiado
+            pc_atual_in        => s_pc_out,      -- vem do PC
+            const_5bit_in      => s_const_5bit,  -- vem do IR fatiado
+            const_13bit_in     => s_const_13bit, -- vem do IR fatiado
+            pc_wr_en_out       => s_pc_wr_en,    -- vai para PC
+            ir_wr_en_out       => s_ir_wr_en,    -- vai para IR
+            reg_wr_en_out      => s_reg_wr_en,   -- vai para Banco
+            ula_chave_out      => s_ula_chave,   -- vai para ULA
+            sel_mux_ula_b_out  => s_sel_mux_ula_b, -- vai para controle do MUX B ULA
+            sel_mux_reg_wr_out => s_sel_mux_reg_wr, -- vai para controle do MUX Escrita Banco
+            pc_in_out          => s_next_pc      -- vai para entrada PC
+        );
+
+
 
 end architecture a_processador;
